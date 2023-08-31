0ddcdd3f0481c697167adee408bf33e6e8edd412
Elijah Wilson-Kelly <elijahwilsonkelly@gmail.com>
2023-08-31 16:57:42 +0200

    fixed typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
-const port = provess.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 server.use(middleware);
 server.use(router);
